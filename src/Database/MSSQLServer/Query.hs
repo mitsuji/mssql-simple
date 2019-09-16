1f3cc60a8bbe9075ff443d965a7026e4b79e5407
mitsuji <tkms@mitsuji.org>
2019-09-16 14:00:19 +0900

    mod: improve documents

--- src/Database/MSSQLServer/Query.hs
+++ src/Database/MSSQLServer/Query.hs
@@ -4,12 +4,15 @@
 -- |
 -- SQL Server client library implemented in Haskell
 --
+-- 'sql' function is useful for execution of plain sql text.
+-- Also 'rpc' function is useful for stored procedure RPC with input/output parameters.
+--
 -- [Usage Example](https://github.com/mitsuji/mssql-simple-example/blob/master/app/Main.hs)
 
 
 module Database.MSSQLServer.Query (
                                   -- * SQL Text Query
-                                  -- $use
+                                  -- $use_sql
                                     sql
                                     
                                   -- ** ResultSet
@@ -21,6 +24,7 @@ module Database.MSSQLServer.Query (
                                   , ReturnStatus (..)
                                   
                                   -- * RPC Query
+                                  -- $use_rpc
                                   , rpc
                                   
                                   -- ** RpcResponseSet
@@ -41,6 +45,7 @@ module Database.MSSQLServer.Query (
                                   , RpcParam (..)
                                   , RpcParamName
 
+                                  -- ** Helpers for passing parameters
                                   , bitVal
                                   , tinyintVal
                                   , smallintVal
@@ -209,7 +214,7 @@ readMessage sock decoder = do
 
 
 
--- $use
+-- $use_sql
 -- A 'sql' function accepts valid 'Connection' and SQL text.
 -- And the expression could be evaluated as a instance of 'ResultSet' type class.
 --
@@ -222,31 +227,21 @@ readMessage sock decoder = do
 -- > import Database.MSSQLServer.Connection
 -- > import Database.MSSQLServer.Query
 -- >
--- > query_select1 :: Connection -> IO Int
--- > query_select1 conn = do
+-- > sql_select1 :: Connection -> IO Int
+-- > sql_select1 conn = do
 -- >     [Only i] <- sql conn "SELECT 2 + 2" :: IO [Only Int]
 -- >     return i
 --
 -- In SQL , uncomputable expression could be evaluated as NULL.
 -- In that case, 'Maybe' type could be used.
 --
--- > {-# LANGUAGE OverloadedStrings #-}
--- >
--- > import Database.MSSQLServer.Connection
--- > import Database.MSSQLServer.Query
--- >
--- > query_select2 :: Connection -> IO (Maybe Int)
--- > query_select2 conn = do
+-- > sql_select2 :: Connection -> IO (Maybe Int)
+-- > sql_select2 conn = do
 -- >     [Only mi] <- sql conn "SELECT 6 / 2" :: IO [Only (Maybe Int)]
 -- >     return mi
 --
--- > {-# LANGUAGE OverloadedStrings #-}
--- >
--- > import Database.MSSQLServer.Connection
--- > import Database.MSSQLServer.Query
--- >
--- > query_select3 :: Connection -> IO (Maybe Int)
--- > query_select3 conn = do
+-- > sql_select3 :: Connection -> IO (Maybe Int)
+-- > sql_select3 conn = do
 -- >     [Only mi] <- sql conn "SELECT 6 / 0" :: IO [Only (Maybe Int)]
 -- >     return mi
 --
@@ -263,20 +258,12 @@ readMessage sock decoder = do
 -- > import Database.Tds.Message
 -- > import Data.Time (UTCTime(..))
 -- >
--- > query_select4 :: Connection -> IO [(Int,String,LT.Text,Money,UTCTime,Maybe UTCTime,Maybe UTCTime)]
--- > query_select4 conn = sql conn "SELECT * FROM TSome ORDER BY somePrice"
+-- > sql_select4 :: Connection -> IO [(Int,String,LT.Text,Money,UTCTime,Maybe UTCTime,Maybe UTCTime)]
+-- > sql_select4 conn = sql conn "SELECT * FROM TSome ORDER BY somePrice"
 --
 -- Any type could be a instance of 'Row' and used as the result of select query as follows.
 --
--- > {-# LANGUAGE OverloadedStrings #-}
 -- > {-# LANGUAGE BangPatterns #-}
--- >
--- > import Database.MSSQLServer.Connection
--- > import Database.MSSQLServer.Query
--- >
--- > import qualified Data.Text.Lazy as LT
--- > import Database.Tds.Message
--- > import Data.Time (UTCTime(..))
 -- >
 -- > data Some = Some { someID :: Int
 -- >                  , someTitle :: LT.Text
@@ -304,29 +291,29 @@ readMessage sock decoder = do
 -- >
 -- >   fromListOfRawBytes _ _ = error "fromListOfRawBytes: List length must be 7"
 -- >
--- > query_select5 :: Connection -> IO [Some]
--- > query_select5 conn = sql conn "SELECT TOP 10 * FROM TSome ORDER BY somePrice DESC"
+-- > sql_select5 :: Connection -> IO [Some]
+-- > sql_select5 conn = sql conn "SELECT TOP 10 * FROM TSome ORDER BY somePrice DESC"
 --
 -- 'Row' is also a instance of 'Result' and tuple of 'Result' is a instance of 'ResultSet'.
 -- So the result of multiple SQL query could be obtained simultaneously as follows.
 --
 -- > import Data.Monoid (mconcat)
 -- >
--- > query_select6 :: Connection -> IO ([Some],[Some])
--- > query_select6 conn =
+-- > sql_select6 :: Connection -> IO ([Some],[Some])
+-- > sql_select6 conn =
 -- >   sql conn $ mconcat ["SELECT * FROM TSome WHERE someID < 8 ORDER BY someID;",
 -- >                       "SELECT * FROM TSome WHERE 8 <= someID AND someID < 12 ORDER BY someID DESC;"]
 --
 -- 'RowCount' is a instance of 'ResultSet'.
--- So the count of SQL table record affected with the SQL query could be obtainde as follows.
+-- So the count of SQL table record affected with the SQL query could be obtained as follows.
 --
 -- > {-# LANGUAGE OverloadedStrings #-}
 -- >
 -- > import Database.MSSQLServer.Connection
 -- > import Database.MSSQLServer.Query
 -- >
--- > query_count1 :: Connection -> IO Int
--- > query_count1 conn = do
+-- > sql_count1 :: Connection -> IO Int
+-- > sql_count1 conn = do
 -- >   RowCount rc <- sql conn "UPDATE TSome SET somePrice = somePrice + 100 WHERE someID < 5"
 -- >   return rc
 --
@@ -338,8 +325,57 @@ readMessage sock decoder = do
 -- > import Database.MSSQLServer.Connection
 -- > import Database.MSSQLServer.Query
 -- >
--- > query_discard1 :: Connection -> IO ()
--- > query_discard1 conn = sql conn "UPDATE TSome SET somePrice = somePrice + 100 WHERE someID < 5"
+-- > sql_discard1 :: Connection -> IO ()
+-- > sql_discard1 conn = sql conn "UPDATE TSome SET somePrice = somePrice + 100 WHERE someID < 5"
+--
+-- Even DROP/CREATE TABLE statement could be executed as follows when your account has privileges.
+--
+-- > {-# LANGUAGE OverloadedStrings #-}
+-- > {-# LANGUAGE QuasiQuotes #-}
+-- >
+-- > import Database.MSSQLServer.Connection
+-- > import Database.MSSQLServer.Query
+-- > import Text.RawString.QQ (r)
+-- >
+-- > sql_create_table1 :: Connection -> IO ()
+-- > sql_create_table1 conn = do
+-- >   sql conn "DROP TABLE TSome" :: IO ()
+-- >   sql conn [r|
+-- > CREATE TABLE TSome (
+-- > 	someID int IDENTITY(1,1) NOT NULL,
+-- > 	someTitle nvarchar(40) NOT NULL,
+-- > 	someContent ntext NOT NULL,
+-- > 	somePrice money NOT NULL,
+-- > 	someCreated datetime NOT NULL,
+-- > 	someModified datetime NULL,
+-- > 	someDeleted datetime NULL,
+-- >
+-- > 	CONSTRAINT PK_TSome PRIMARY KEY CLUSTERED (
+-- > 		someID ASC
+-- > 	)
+-- > 	WITH (
+-- > 		PAD_INDEX  = OFF,
+-- > 		STATISTICS_NORECOMPUTE  = OFF,
+-- > 		IGNORE_DUP_KEY = OFF,
+-- > 		ALLOW_ROW_LOCKS  = ON,
+-- > 		ALLOW_PAGE_LOCKS  = ON
+-- > 	) ON [PRIMARY]
+-- >
+-- > ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+-- > |]
+--
+-- INSERT batch could be executed within transaction as follows.
+--
+-- > sql_insert1 :: Connection -> IO ()
+-- > sql_insert1 conn = sql conn [r|
+-- > BEGIN TRAN;
+-- > INSERT INTO TSome(someTitle,someContent,somePrice,someCreated) VALUES('title','content',12345.60,GETDATE());
+-- > INSERT INTO TSome(someTitle,someContent,somePrice,someCreated) VALUES('title','content',12345.60,GETDATE());
+-- > INSERT INTO TSome(someTitle,someContent,somePrice,someCreated) VALUES('title','content',12345.60,GETDATE());
+-- > INSERT INTO TSome(someTitle,someContent,somePrice,someCreated) VALUES('title','content',12345.60,GETDATE());
+-- > COMMIT TRAN;
+-- > |]
+-- >
 --
 -- 'ReturnStatus' is a instance of 'ResultSet'.
 -- So when a stored procedure executed in SQL text, Return status of the execution could be obtained as follows.
@@ -349,7 +385,41 @@ readMessage sock decoder = do
 -- > import Database.MSSQLServer.Connection
 -- > import Database.MSSQLServer.Query
 -- >
--- > query_status1 :: Connection -> IO Int
--- > query_status1 conn = do
+-- > sql_status1 :: Connection -> IO Int
+-- > sql_status1 conn = do
 -- >   ReturnStatus rets <- sql conn "EXEC SP_Input1 @Val1=3"
 -- >   return rets
+
+
+-- $use_rpc
+-- Stored procedure RPC could be executed as follows.
+--
+-- > {-# LANGUAGE OverloadedStrings #-}
+-- >
+-- > import Database.MSSQLServer.Connection
+-- > import Database.MSSQLServer.Query
+-- >
+-- > import qualified Data.Text as T
+-- > import Data.Word (Word16)
+-- >
+-- > rpc_sql_select1 :: Connection -> IO (RpcResponse () [Some])
+-- > rpc_sql_select1 conn = rpc conn $
+-- >                        RpcQuery ("sp_executesql"::T.Text) $ nvarcharVal "" (Just "SELECT * FROM TSome")
+--
+-- There several ways provided for specify stored procedures.
+-- See ProcID section of [\[MS-TDS\] 2.2.6.6 RPC Request](https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-tds/619c43b6-9495-4a58-9e49-a4950db245b3).
+--
+-- > rpc_sql_select2 :: Connection -> Int -> IO (RpcResponse () [Some])
+-- > rpc_sql_select2 conn max = rpc conn $ RpcQuery SP_ExecuteSql
+-- >                            ( nvarcharVal "" (Just "SELECT * FROM TSome WHERE someID < @Max")
+-- >                            , nvarcharVal "" (Just "@Max Int")
+-- >                            , intVal "" (Just max)
+-- >                            )
+--
+-- > rpc_sql_select3 :: Connection -> Int -> Int -> IO (RpcResponse () [Some])
+-- > rpc_sql_select3 conn min max = rpc conn $ RpcQuery (0xa::Word16)
+-- >                                ( nvarcharVal "" (Just "SELECT * FROM TSome WHERE @Min < someID AND someID < @Max")
+-- >                                , nvarcharVal "" (Just "@Min Int, @Max Int")
+-- >                                , intVal "@Min" (Just min)
+-- >                                , intVal "@Max" (Just max)
+-- >                                )
